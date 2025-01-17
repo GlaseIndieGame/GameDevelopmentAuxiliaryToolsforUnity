8418644758008379dfd09a07c4bf72b85008e439
DESKTOP-67MT2K8\user <dessermuhine@gmail.com>
2025-01-17 09:41:22 +0900

    ## [1.3.3] - 2025-01-17 ### Fixed - カスタムテンプレートスクリプトの誤字を修正

--- Editor/UserCustom/CustomMenuScriptsSettings.cs
+++ Editor/UserCustom/CustomMenuScriptsSettings.cs
@@ -10,7 +10,7 @@ namespace GDAT.Editor
     internal class CustomMenuScriptsSettings : UnityEditor.Editor
     {
         private const string SCRIPT_MENU_ROOT = "Assets/Tools/GDATforUnity/Add New Script/";
-        private const string ESCRIPT_CRIPT_MENU_ROOT = "Assets/Tools/GDATforUnity/Add New Editor Script/";
+        private const string EDITOR_SCRIPT_MENU_ROOT = "Assets/Tools/GDATforUnity/Add New Editor Script/";
 
         private const int SCRIPT_PRIORITY = 15;
         private const int EDITOR_SCRIPT_PRIORITY = 17;
